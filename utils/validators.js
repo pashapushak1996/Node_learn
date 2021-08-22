d927d5f6a8ac9c540333d8fc99d41aebcf1d8c08
pashapushak1996 <pashapushak1996@gmail.com>
2021-08-22 18:07:47 +0300

    validator fix

--- utils/validators.js
+++ utils/validators.js
@@ -10,8 +10,7 @@ const userDataValidator = (email, password) => {
 
     const isValidEmail = domains.includes(domain) && email.includes('@');
     const isValidPassword = password.length > 0;
-
-    if (!isValidEmail && !isValidPassword) {
+    if (!isValidEmail || !isValidPassword) {
         return null;
     }
 
