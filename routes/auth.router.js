dc0c378cb6991b78a113caf00b5615d975cf4214
pashapushak1996 <pashapushak1996@gmail.com>
2021-08-22 17:37:07 +0300

    register page

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { authController } = require('../controllers');
 
 router.post('/', authController.login);
+
 router.get('/register', authController.register);
 
 module.exports = router;
