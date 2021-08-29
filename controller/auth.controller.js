a5cd0139ff929be0893c6634ffc8e77823f1dfdc
pashapushak1996 <pashapushak1996@gmail.com>
2021-08-29 08:44:27 +0300

    almost done

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,22 +1,19 @@
 const { ErrorHandler, errorMessages } = require('../error');
-const { User } = require('../dataBase');
 const { passwordService } = require('../service');
 const { statusCodesEnum } = require('../config');
 
 const authController = {
     login: async (req, res, next) => {
         try {
-            const { password, email } = req.body;
-
-            const currentUser = await User.findOne({ email });
+            const { body: { password, email }, currentUser } = req;
 
             const isTrueEmail = email === currentUser.email;
-
-            await passwordService.comparePassword(password, currentUser.password);
 
             if (!isTrueEmail) {
                 throw new ErrorHandler(statusCodesEnum.NOT_FOUND, errorMessages.NOT_FOUND_USER);
             }
+
+            await passwordService.comparePassword(password, currentUser.password);
 
             res.json('User logged');
         } catch (e) {
